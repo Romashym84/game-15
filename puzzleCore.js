f24100649be23c6b1bfbf7a37357a9e0360c0f81
Roman <romashym84@gmail.com>
2024-12-09 15:36:18 +0200

    init

---
+++ puzzleCore.js
@@ -0,0 +1,176 @@
+// const FIELD_SIZE = 4;
+const EMPTY_VALUE = null;
+const MOVE_DIRECTION = {
+  UP: "up",
+  DOWN: "down",
+  LEFT: "left",
+  RIGHT: "right",
+};
+const gameConfig = { size: 4 };
+
+let state = [];
+
+function setSize(size) {
+  gameConfig.size = size;
+}
+
+function setInitialState(size) {
+  const maxCount = size * size;
+  const result = [];
+
+  for (let i = 1; i < maxCount; i++) {
+    result.push(i);
+  }
+
+  result.push(EMPTY_VALUE);
+
+  return result;
+}
+
+function getIndexByCoord(x, y) {
+  let index = x + y * gameConfig.size;
+  return index;
+}
+
+function getValueByIndex(index) {
+  return state[index];
+}
+
+function getCoordByIndex(index) {
+  const y = Math.floor(index / gameConfig.size);
+  const x = index - y * gameConfig.size;
+  return { x, y };
+}
+
+function getCoordByValue(state, value) {
+  return getCoordByIndex(state.indexOf(value));
+}
+
+function getCoordByEmpty(state) {
+  return getCoordByValue(state, EMPTY_VALUE);
+}
+
+function checkMoove(state, value) {
+  const valueCoord = getCoordByValue(state, value);
+  const emptyCoord = { ...getCoordByEmpty(state), direction: null };
+
+  if (valueCoord.x === emptyCoord.x && valueCoord.y - 1 === emptyCoord.y) {
+    console.log("up");
+    emptyCoord.direction = MOVE_DIRECTION.UP;
+    return emptyCoord;
+  }
+
+  if (valueCoord.x === emptyCoord.x && valueCoord.y + 1 === emptyCoord.y) {
+    console.log("down");
+    emptyCoord.direction = MOVE_DIRECTION.DOWN;
+    return emptyCoord;
+  }
+
+  if (valueCoord.x - 1 === emptyCoord.x && valueCoord.y === emptyCoord.y) {
+    console.log("left");
+    emptyCoord.direction = MOVE_DIRECTION.LEFT;
+    return emptyCoord;
+  }
+
+  if (valueCoord.x + 1 === emptyCoord.x && valueCoord.y === emptyCoord.y) {
+    console.log("right");
+    emptyCoord.direction = MOVE_DIRECTION.RIGHT;
+    return emptyCoord;
+  }
+}
+
+function move(state, value) {
+  //   debugger;
+  const coord = checkMoove(state, value);
+  if (!coord) return;
+  const index = state.indexOf(value);
+  const newIndex = getIndexByCoord(coord.x, coord.y);
+  [state[index], state[newIndex]] = [state[newIndex], state[index]];
+
+  return coord.direction;
+}
+
+function checkState(state) {
+  return state.every(
+    (value, index) => index === value - 1 || value === EMPTY_VALUE
+  );
+}
+
+function getActiveValue(state) {
+  const result = [];
+  const coordEmpty = getCoordByEmpty(state);
+  const upIndex = getIndexByCoord(coordEmpty.x, coordEmpty.y - 1);
+  if (coordEmpty.y !== 0) {
+    result.push(state[upIndex]);
+  }
+
+  const downIndex = getIndexByCoord(coordEmpty.x, coordEmpty.y + 1);
+  if (coordEmpty.y !== gameConfig.size - 1) {
+    result.push(state[downIndex]);
+  }
+
+  const leftIndex = getIndexByCoord(coordEmpty.x - 1, coordEmpty.y);
+  if (coordEmpty.x !== 0) {
+    result.push(state[leftIndex]);
+  }
+
+  const rightIndex = getIndexByCoord(coordEmpty.x + 1, coordEmpty.y);
+  if (coordEmpty.x !== gameConfig.size - 1) {
+    result.push(state[rightIndex]);
+  }
+
+  return result;
+}
+
+function getRandomValue(maxValue) {
+  return Math.floor(Math.random() * (maxValue + 1));
+}
+
+function getRandomActiveValue(state) {
+  const activeValue = getActiveValue(state);
+  const index = getRandomValue(activeValue.length - 1);
+  return activeValue[index];
+}
+
+function refresh() {
+  state = setInitialState(gameConfig.size)
+    .map((item) => ({ item, index: Math.random() }))
+    .sort((a, b) => a.index - b.index)
+    .map(({ item }) => item);
+  if (!checkInversion(state)) {
+    refresh();
+    console.log("repeat refresh");
+  }
+}
+
+// function refresh() {
+//   state = setInitialState(gameConfig.size);
+// }
+
+function initGame() {
+    state = setInitialState(gameConfig.size);
+//   refresh();
+}
+
+function checkInversion(state) {
+  const stateNoEmpty = state.filter((state) => state !== EMPTY_VALUE);
+  const emptyY = getCoordByEmpty(state).y + 1;
+  console.log(emptyY);
+  let inversion = 0;
+  for (let i = 0; i < stateNoEmpty.length; i++) {
+    for (let j = i + 1; j < stateNoEmpty.length; j++) {
+      if (stateNoEmpty[i] > stateNoEmpty[j]) {
+        inversion++;
+      }
+    }
+  }
+  if (isEven(gameConfig.size)) {
+    return isEven(inversion + emptyY);
+  } else {
+    return isEven(inversion);
+  }
+}
+
+function isEven(value) {
+  return value % 2 === 0;
+}
---
+++ script.js
@@ -0,0 +1,166 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const box = document.querySelector(".box");
+  const field = document.querySelector(".field");
+  const range = document.getElementById("rangeInput");
+  const rangeValue = document.getElementById("rangeValue");
+  const resetButton = document.querySelector(".reset");
+  const countDisplay = document.getElementById("count_num");
+  const timerDisplay = document.getElementById("timer_num");
+
+  let showCount = 0;
+  let timer = null;
+  let startTime = null;
+  let isActiveAnimation = false;
+  let isActiveGame = false;
+  //   let sizeField;
+
+  function startTimer() {
+    if (!startTime) {
+      startTime = new Date();
+      timer = setInterval(updateTimer, 1000);
+    }
+  }
+
+  function updateTimer() {
+    const now = new Date();
+    let elapsedTime = Math.floor((now - startTime) / 1000);
+
+    let minutes = Math.floor(elapsedTime / 60);
+    let seconds = elapsedTime % 60;
+
+    const formatMinutes = (minutes < 10 ? "0" : "") + minutes;
+    const formatSeconds = (seconds < 10 ? "0" : "") + seconds;
+
+    timerDisplay.textContent = formatMinutes + ":" + formatSeconds;
+  }
+
+  function stopTimer() {
+    if (timer !== null) {
+      clearInterval(timer);
+      timer = null;
+    }
+  }
+
+  function resetAll() {
+    showCount = 0;
+    countDisplay.textContent = "0";
+    stopTimer();
+    startTime = null;
+    timerDisplay.textContent = "00:00";
+  }
+
+  function initStyle(size) {
+    const value = `repeat(${size}, 1fr)`;
+    field.style = `grid-template-columns:${value};grid-template-rows: ${value};`;
+  }
+
+  function init() {
+    initStyle(gameConfig.size);
+    initGame();
+    updateRender();
+  }
+
+  function updateRender() {
+    render(state, field);
+  }
+
+  resetButton.addEventListener("click", () => {
+    isActiveGame = false;
+    resetAll();
+    init();
+  });
+
+  init();
+
+  field.addEventListener("click", (event) => {
+    if (isActiveAnimation) return;
+    event.preventDefault();
+    console.log(event.defaultPrevented);
+
+    // if (checkState(state)) return;
+
+    const target = event.target;
+
+    if (target.classList.contains("number")) {
+      console.log(target.textContent);
+      const direction = move(state, Number.parseInt(target.textContent));
+
+      if (!direction) return;
+
+      target.classList.add(`number--${direction}`);
+      isActiveAnimation = true;
+
+      if (!isActiveGame) {
+        startTimer();
+        isActiveGame = true;
+      }
+
+      setTimeout(() => {
+        updateRender();
+        countDisplay.textContent = ++showCount;
+        console.log(showCount);
+        if (checkState(state)) {
+          alert("ОТ МОЛОДЕЦЬ!!!");
+          stopTimer();
+        }
+
+        isActiveAnimation = false;
+      }, 250);
+    }
+  });
+
+  range.addEventListener("input", (event) => {
+    const size = event.target.value;
+    console.log(size);
+    rangeValue.textContent = size;
+    setSize(size);
+    init();
+    console.log(gameConfig.size);
+    box.style.maxWidth = resizeField(gameConfig.size);
+  });
+
+  function resizeField(size) {
+    if (size === "3") {
+      return (box.style.maxWidth = "366px");
+    } else if (size === "4") {
+      return (box.style.maxWidth = "443px");
+    } else if (size === "5") {
+      return (box.style.maxWidth = "560px");
+    } else if (size === "6") {
+      return (box.style.maxWidth = "645px");
+    }
+  }
+});
+
+function render(list, container) {
+  container.innerHTML = "";
+
+  list.forEach((item) => {
+    const elem = document.createElement("div");
+    elem.classList.add("number");
+
+    if (!item) {
+      elem.classList.add("number--empty");
+    }
+
+    elem.setAttribute("id", `num_${item}`);
+    elem.textContent = item;
+
+    container.insertAdjacentElement("beforeend", elem);
+  });
+}
+
+function autoRun(count) {
+  let curentCount = 0;
+  const id = setInterval(() => {
+    const value = getRandomActiveValue(state);
+    const elem = document.getElementById(`num_${value}`);
+
+    if (elem) {
+      elem.click();
+    }
+    console.log(curentCount, value, elem);
+    curentCount++;
+    if (curentCount === count) clearInterval(id);
+  }, 300);
+}
